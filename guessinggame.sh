cbb6f5743066f6154a60acc5ae9eb10f677a8815
lioncitydevops <leetzehoung@gmail.com>
2022-01-07 15:49:34 +0800

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+# Peer graded assignment for Coursera/JHU The Unix Workbench
+
+count=$(ls | wc -l)
+
+function correct_guess {        # One function...
+    if [[ $guess -lt $count ]]          # One if statement ...
+    then
+	echo "Too low!"
+    elif [[ $guess -gt $count ]]
+    then
+	echo "Too high!"
+    else
+	echo ""
+	echo "Congratualations! You guessed right, there are $count file(s) in $PWD "
+    fi
+}
+
+while true
+do
+    read -p "Guess how many files are in the current directory? " guess
+    echo $(correct_guess)
+    echo ""
+    if [[ $guess -ne $count ]]
+    then
+    	continue
+	else
+		break
+	fi
+done
